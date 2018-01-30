8aedf5f7877e93ecec46ab078f3cd7107d1466a4
LorandVeres <lorand.mast@gmail.com>
2018-01-30 17:21:20 +0000

    fixing triger in dialog and html object to dialog body

--- darddialog.js
+++ darddialog.js
@@ -15,99 +15,115 @@ var dialog = {
             d.setAttribute('id', id);
             h.setAttribute('id', 'dard_d_hd');
             b.setAttribute('id', 'dard_d_cl');
-            b.appendChild(document.createTextNode('❌'));
+            b.appendChild(document.createTextNode('X'));
             h.appendChild(b);
             d.appendChild(h);
             d.appendChild(bd);
             d.appendChild(f);
-            
+
             document.body.appendChild(d);
         };
-        return (function() {
+        return ( function() {
                 create();
                 return [d, h, b, bd, f];
             }());
 
     },
-    
-    setClass : function(elements, obj){
-        if(MyObj.keyIn(obj, 'css')){
-            for(var i = 0, j = elements.length; i < j; i ++ ){
-                elements[i].className += ' '+ obj.css[i];
+
+    setClass : function(elements, obj) {
+        if (MyObj.keyIn(obj, 'css')) {
+            for (var i = 0,
+                j = elements.length; i < j; i++) {
+                elements[i].className += ' ' + obj.css[i];
             }
         }
     },
-    
-    appendBody : function (body, obj){
-        if(MyObj.keyIn(obj, 'html')){
-            var el = str2el(obj.html);
-            body.appendChild(el);
+
+    appendBody : function(body, obj) {
+        if (MyObj.keyIn(obj, 'html')) {
+            if(typeof obj.html === 'string'){
+                var el = str2el(obj.html);
+                body.appendChild(el);
+            }
+            if(typeof obj.html == 'object'){
+                if(obj.html){
+                    var el = obj.html.cloneNode(true);
+                    body.appendChild(obj.html);
+                }
+            }
         }
     },
-    
-    appendTitle : function (head, obj){
-        if(MyObj.keyIn(obj, 'title')){
+
+    appendName : function(head, obj) {
+        if (MyObj.keyIn(obj, 'title')) {
             var n = document.createElement('h4');
             n.appendChild(document.createTextNode(obj.title));
             head.appendChild(n);
         }
     },
-    
-    clearBody : function(el){
+
+    clearBody : function(el) {
         empty(el);
     },
-    
-    myEvents : function (obj){
-        if(MyObj.keyIn(obj, 'acton')){
-            for(var i=0,j=obj.acton.myevent.length; i<j; i++){
-              $(obj.acton.triger[i]).addEventListener(obj.acton.myevent[i], obj.acton.handler[i], false);
+
+    myEvents : function(obj) {
+        if (MyObj.keyIn(obj, 'acton')) {
+            for (var i = 0,
+                j = obj.acton.myevent.length; i < j; i++) {
+                $(obj.acton.triger[i]).addEventListener(obj.acton.myevent[i], obj.acton.handler[i], false);
             };
         }
     },
-    
+
     init : function(triger, obj) {
-        triger.addEventListener('click', function() {
-            var el = dialog.mywindow() ,
-                d = el[0],
-                h = el[1],
-                b = el[2],
-                bd = el[3],
-                f = el[4],
-                x,
-                y;
-                
-            dialog.setClass(el, obj);
-            dialog.appendTitle(h, obj);
-            dialog.appendBody(bd, obj);
-                                    
-            h.addEventListener('mousedown', mouseDown, false);
-            f.addEventListener('mousedown', mouseDown, false);
-            window.addEventListener('mouseup', mouseUp, false);
-            b.addEventListener('click', function() {
-                document.body.removeChild(d);
+        if (triger) {
+            triger.addEventListener('click', function() {
+                var el = dialog.mywindow(),
+                    d = el[0],
+                    h = el[1],
+                    b = el[2],
+                    bd = el[3],
+                    f = el[4],
+                    x,
+                    y;
+
+                dialog.setClass(el, obj);
+                dialog.appendName(h, obj);
+                dialog.appendBody(bd, obj);
+
+                h.addEventListener('mousedown', mouseDown, false);
+                f.addEventListener('mousedown', mouseDown, false);
+                window.addEventListener('mouseup', mouseUp, false);
+                b.addEventListener('click', function() {
+                    document.body.removeChild(d);
+                });
+
+                function getStyle(e, s) {
+                    return window.getComputedStyle(e, null).getPropertyValue(s);
+                }
+
+                function mouseDown(e) {
+                    window.addEventListener('mousemove', divMove, true);
+                    y = e.clientY - getStyle(d, 'top').slice(0, -2),
+                    x = e.clientX - getStyle(d, 'left').slice(0, -2);
+                }
+
+                function mouseUp() {
+                    window.removeEventListener('mousemove', divMove, true);
+                }
+
+                function divMove(e) {
+                    //d.style.position = 'absolute';
+                    d.style.top = (e.clientY - y) + 'px';
+                    d.style.left = (e.clientX - x) + 'px';
+                }
+
+
+                dialog.myEvents(obj);
             });
+        }else{
+            console.log(triger + ' dialog content not on this page');
+        }
 
-            function getStyle(e, s) {
-                return window.getComputedStyle(e, null).getPropertyValue(s);
-            }
-
-            function mouseDown(e) {
-                window.addEventListener('mousemove', divMove, true);
-                y = e.clientY - getStyle(d, 'top').slice(0, -2),
-                x = e.clientX - getStyle(d, 'left').slice(0, -2);
-            }
-
-            function mouseUp() {
-                window.removeEventListener('mousemove', divMove, true);
-            }
-
-            function divMove(e) {
-                //d.style.position = 'absolute';
-                d.style.top = (e.clientY - y) + 'px';
-                d.style.left = (e.clientX - x) + 'px';
-            }
-            dialog.myEvents(obj);
-        });
-        
     }
 };
