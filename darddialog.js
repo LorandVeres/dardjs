3493ee9145b511592e4e81b0379f440dbe57241e
LorandVeres <lorand.mast@gmail.com>
2018-02-06 18:29:03 +0000

    working sample of draggable dialog before changes

--- darddialog.js
+++ darddialog.js
@@ -1,133 +1,221 @@
 var dialog = {
 
-    mywindow : function() {
+	mywindow : function() {
+		var d = document.createElement('div'),
+		    h = document.createElement('div'),
+		    ct = document.createElement('div'),
+		    bt = document.createElement('div'),
+		    m = document.createElement('div'),
+		    mx = document.createElement('div'),
+		    bd = document.createElement('div'),
+		    f = document.createElement('div'),
+		    uniq = new Date().getTime(),
+		    id = 'dard_d_bd' + uniq;
 
-        var d = document.createElement('div'),
-            h = document.createElement('div'),
-            b = document.createElement('div'),
-            bd = document.createElement('div'),
-            f = document.createElement('div'),
-            uniq = new Date().getTime(),
-            id = 'dard_d_bd' + uniq;
+		function create() {
 
-        function create() {
+			ct.appendChild(bt);
+			ct.appendChild(mx);
+			ct.appendChild(m);
+			h.appendChild(ct);
+			d.appendChild(h);
+			d.appendChild(bd);
+			d.appendChild(f);
+			d.setAttribute('id', id);
+			d.setAttribute('class', 'dard_d_box');
+			h.setAttribute('class', 'dard_d_hd');
+			ct.setAttribute('class', 'dard_d_ct');
+			bt.setAttribute('class', 'dard_d_bt');
+			m.setAttribute('class', 'dard_d_bt');
+			mx.setAttribute('class', 'dard_d_bt');
+			bd.setAttribute('class', 'dard_d_bd');
+			f.setAttribute('class', 'dard_d_f');
+			m.innerHTML = '&#95';
+			mx.innerHTML = '&square;';
+			bt.innerHTML = '&times;';
 
-            d.setAttribute('id', id);
-            h.setAttribute('id', 'dard_d_hd');
-            b.setAttribute('id', 'dard_d_cl');
-            b.innerHTML = '&times;';
-            h.appendChild(b);
-            d.appendChild(h);
-            d.appendChild(bd);
-            d.appendChild(f);
+			document.body.appendChild(d);
+		};
+		return ( function() {
+				create();
+				return [d, h, bt, m, mx, bd, f];
+			}());
 
-            document.body.appendChild(d);
-        };
-        return ( function() {
-                create();
-                return [d, h, b, bd, f];
-            }());
+	},
 
-    },
+	toggle : function() {
+		var t = document.createElement('div');
+		t.setAttribute('class', 'dialog-toggle');
+		t.innerHTML = '+';
+		document.body.appendChild(t);
+		return t;
+	},
 
-    setClass : function(elements, obj) {
-        if (MyObj.keyIn(obj, 'css')) {
-            for (var i = 0,
-                j = elements.length; i < j; i++) {
-                elements[i].className += ' ' + obj.css[i];
-            }
-        }
-    },
+	setClass : function(elements, obj) {
+		if (obj.keyIn('css')) {
+			var css = obj.css;
+		}
+		if (isArray(obj)) {
+			var css = obj;
+		}
+		if (css) {
+			for (var i = 0,
+			    j = elements.length; i < j; i++) {
+				if ( typeof css[i] === 'string' && css[i].lenght > 1) {
+					elements[i].removeAttribute("class");
+				}
+				elements[i].setAttribute('class', css[i]);
+			}
+		}
+	},
 
-    appendBody : function(body, obj) {
-        if (MyObj.keyIn(obj, 'html')) {
-            if ( typeof obj.html === 'string') {
-                var el = str2el(obj.html);
-                body.appendChild(el);
-            }
-            if ( typeof obj.html == 'object') {
-                if (obj.html) {
-                    var el = obj.html.cloneNode(true);
-                    body.appendChild(obj.html);
-                }
-            }
-        }
-    },
+	appendBody : function(body, obj) {
+		if (obj.keyIn('html')) {
+			if ( typeof obj.html === 'string') {
+				var el = str2el(obj.html);
+				body.appendChild(el);
+			}
+			if ( typeof obj.html == 'object') {
+				if (obj.html) {
+					var el = obj.html.cloneNode(true);
+					body.appendChild(obj.html);
+				}
+			}
+		}
+	},
 
-    appendName : function(head, obj) {
-        if (MyObj.keyIn(obj, 'title')) {
-            var n = document.createElement('h4');
-            n.appendChild(document.createTextNode(obj.title));
-            head.appendChild(n);
-        }
-    },
+	appendName : function(head, obj) {
+		if (obj.keyIn('title')) {
+			var n = document.createElement('h4');
+			n.appendChild(document.createTextNode(obj.title));
+			head.appendChild(n);
+		}
+	},
 
-    clearBody : function(el) {
-        empty(el);
-    },
+	clearBody : function(el) {
+		emptyEl(el);
+	},
 
-    myEvents : function(obj) {
-        if (MyObj.keyIn(obj, 'acton')) {
-            for (var i = 0,
-                j = obj.acton.myevent.length; i < j; i++) {
-                $(obj.acton.triger[i]).addEventListener(obj.acton.myevent[i], obj.acton.handler[i], false);
-            };
-        }
-    },
+	myEvents : function(obj) {
+		if (obj.keyIn('acton')) {
+			for (var i = 0,
+			    j = obj.acton.myevent.length; i < j; i++) {
+				$(obj.acton.trigger[i]).addEventListener(obj.acton.myevent[i], obj.acton.handler[i], false);
+			};
+		}
+	},
 
-    init : function(triger, obj) {
-        var single = counter();
-        if (triger) {
-            triger.addEventListener('click', function() {
-                if (single.show() < 1) {
-                    single.increment();
-                    var el = dialog.mywindow(),
-                        d = el[0],
-                        h = el[1],
-                        b = el[2],
-                        bd = el[3],
-                        f = el[4],
-                        x,
-                        y;
+	init : function(trigger, obj) {
+		var single = counter();
+		if (trigger) {
+			trigger.addEventListener('click', function() {
+				if (single.show() < 1) {
+					single.increment();
+					if (obj.sideButton)
+						var t = dialog.toggle();
+					var el = dialog.mywindow(),
+					    d = el[0],
+					    h = el[1],
+					    bt = el[2],
+					    m = el[3],
+					    mx = el[4],
+					    bd = el[5],
+					    f = el[6],
+					    x,
+					    y;
 
-                    dialog.setClass(el, obj);
-                    dialog.appendName(h, obj);
-                    dialog.appendBody(bd, obj);
+					dialog.setClass(el, obj);
+					dialog.appendName(h, obj);
+					dialog.appendBody(bd, obj);
+					//ontouchmove
 
-                    h.addEventListener('mousedown', mouseDown, false);
-                    f.addEventListener('mousedown', mouseDown, false);
-                    window.addEventListener('mouseup', mouseUp, false);
-                    b.addEventListener('click', function() {
-                        document.body.removeChild(d);
-                        single.reset();
-                    });
+					if (t)
+						t.addEventListener('click', maximize, false);
+					m.addEventListener('click', minimize, false);
+					mx.addEventListener('click', maximize, false);
+					bt.addEventListener('click', remove, false);
 
-                    function getStyle(e, s) {
-                        return window.getComputedStyle(e, null).getPropertyValue(s);
-                    }
+					f.addEventListener('mousedown', mouseDown, false);
+					h.addEventListener('mousedown', mouseDown, false);
+					window.addEventListener('mouseup', freeup, false);
+					h.addEventListener('touchmove', touches, false);
+					window.addEventListener('touchend', freeup, false);
 
-                    function mouseDown(e) {
-                        window.addEventListener('mousemove', divMove, true);
-                        y = e.clientY - getStyle(d, 'top').slice(0, -2),
-                        x = e.clientX - getStyle(d, 'left').slice(0, -2);
-                    }
+					function remove() {
+						document.body.removeChild(d);
+						if (obj.sideButton)
+							document.body.removeChild(t);
+						single.reset();
+						h.removeEventListener('mousedown', mouseDown, true);
+						f.removeEventListener('mousedown', mouseDown, true);
+						window.removeEventListener('mouseup', freeup, true);
+						window.removeEventListener('mousedown', freeup, true);
+						bt.removeEventListener('click', remove, true);
+						m.removeEventListener('click', minimize, true);
+					}
 
-                    function mouseUp() {
-                        window.removeEventListener('mousemove', divMove, true);
-                    }
+					function maximize() {
+						if (obj.keyIn('transform')) {
+							if (d.className === obj.transform[0]) {
+								dialog.setClass(el, obj);
+								if (t)
+									t.style.display = 'none';
+							}
+						} else {
+							d.style.display = 'block';
+							if (t)
+								t.style.display = 'none';
+						}
+					}
 
-                    function divMove(e) {
-                        //d.style.position = 'absolute';
-                        d.style.top = (e.clientY - y) + 'px';
-                        d.style.left = (e.clientX - x) + 'px';
-                    }
+					function minimize() {
+						if (obj.keyIn('transform')) {
+							if (d.className === obj.css[0] || d.className === 'dard_d_box') {
+								dialog.setClass([d, h, bd, f], obj.transform);
+								if (t)
+									t.style.display = 'block';
+							}
+						} else {
+							d.style.display = 'none';
+							if (t)
+								t.style.display = 'block';
+						}
+					}
+
+					function touches(event) {
+						var touch = event.targetTouches[0];
+						d.style.left = touch.pageX - 20 + 'px';
+						d.style.top = touch.pageY - 20 + 'px';
+						event.preventDefault();
+					}
+
+					function getStyle(e, s) {
+						return window.getComputedStyle(e, null).getPropertyValue(s);
+					}
+
+					function mouseDown(e) {
+						window.addEventListener('mousemove', divMove, true);
+						y = e.clientY - getStyle(d, 'top').slice(0, -2),
+						x = e.clientX - getStyle(d, 'left').slice(0, -2);
+					}
+
+					function divMove(e) {
+						//d.style.position = 'absolute';
+						d.style.top = (e.clientY - y) + 'px';
+						d.style.left = (e.clientX - x) + 'px';
+					}
+
+					function freeup() {
+						window.removeEventListener('mousemove', divMove, true);
+						window.removeEventListener('touchmove', touches, true);
+					}
 
 
-                    dialog.myEvents(obj);
-                }
-            });
-        } else {
-            console.log(triger + ' dialog content not on this page');
-        }
-    }
+					dialog.myEvents(obj);
+				}
+			});
+		} else {
+			console.log(trigger + ' dialog content not on this page');
+		}
+	}
 };
