60faa69dbd2784936249906afdb9e2fb2d4aca95
LorandVeres <lorand.mast@gmail.com>
2018-02-07 12:29:36 +0000

    darddialog.js up for tagging

--- darddialog.js
+++ darddialog.js
@@ -1,57 +1,103 @@
-var dialog = {
+/*
 
-	mywindow : function() {
-		var d = document.createElement('div'),
-		    h = document.createElement('div'),
-		    ct = document.createElement('div'),
-		    bt = document.createElement('div'),
-		    m = document.createElement('div'),
-		    mx = document.createElement('div'),
-		    bd = document.createElement('div'),
-		    f = document.createElement('div'),
-		    uniq = new Date().getTime(),
-		    id = 'dard_d_bd' + uniq;
+ custom : { wrapper, head, close, max, min, body, footer }
+ onside : html,
+ title : 'Dardjs dialog box',
+ transform : ['wrapper', 'head', 'buttons', 'body', 'footer'],
+ css : ['dard_d_box', 'dard_d_hd', 'dard_d_bt_cl', 'dard_d_bt_min', 'dard_d_bt_max', 'dard_d_bd', 'dard_d_f'],
+ body : html,
+ drag : true || false,
+ acton : {
+ 	myevent : ['click'],
+ 	trigger : [html],
+ 	handler: [ funyFunction ]
+ }
 
-		function create() {
 
-			ct.appendChild(bt);
-			ct.appendChild(mx);
-			ct.appendChild(m);
-			h.appendChild(ct);
-			d.appendChild(h);
-			d.appendChild(bd);
-			d.appendChild(f);
-			d.setAttribute('id', id);
-			d.setAttribute('class', 'dard_d_box');
-			h.setAttribute('class', 'dard_d_hd');
-			ct.setAttribute('class', 'dard_d_ct');
-			bt.setAttribute('class', 'dard_d_bt');
-			m.setAttribute('class', 'dard_d_bt');
-			mx.setAttribute('class', 'dard_d_bt');
-			bd.setAttribute('class', 'dard_d_bd');
-			f.setAttribute('class', 'dard_d_f');
-			m.innerHTML = '&#95';
-			mx.innerHTML = '&square;';
-			bt.innerHTML = '&times;';
+ */
+function dialog (trigger, obj) {
 
-			document.body.appendChild(d);
-		};
-		return ( function() {
+	var $$ = {}, single = counter(), drag,
+		id = 'dard_d_bd' + new Date().getTime();
+		obj.keyIn('drag') && obj.drag == true ? drag = true : drag = false;
+
+	function mywindow(obj) {
+
+		function create(){
+			$$.d = document.createElement('div');
+			$$.h = document.createElement('div');
+			$$.ct = document.createElement('div');
+			$$.bt = document.createElement('div');
+			$$.m = document.createElement('div');
+			$$.mx = document.createElement('div');
+			$$.bd = document.createElement('div');
+			$$.f = document.createElement('div');
+		}
+
+		function append() {
+
+			$$.ct.appendChild($$.bt);
+			$$.ct.appendChild($$.mx);
+			$$.ct.appendChild($$.m);
+			$$.h.appendChild($$.ct);
+			$$.d.appendChild($$.h);
+			$$.d.appendChild($$.bd);
+			$$.d.appendChild($$.f);
+			$$.d.setAttribute('id', id);
+			$$.d.setAttribute('class', 'dard_d_box');
+			$$.h.setAttribute('class', 'dard_d_hd');
+			$$.ct.setAttribute('class', 'dard_d_ct');
+			$$.bt.setAttribute('class', 'dard_d_bt');
+			$$.m.setAttribute('class', 'dard_d_bt');
+			$$.mx.setAttribute('class', 'dard_d_bt');
+			$$.bd.setAttribute('class', 'dard_d_bd');
+			$$.f.setAttribute('class', 'dard_d_f');
+			$$.m.innerHTML = '&#95';
+			$$.mx.innerHTML = '&square;';
+			$$.bt.innerHTML = '&times;';
+
+		}
+		
+		function appendToBody(obj){
+			if(obj.keyIn('custom')){
+				obj.custom.keyIn('head') ? $$.h = obj.custom.head : $$.h = null;
+				obj.custom.keyIn('close') ? $$.bt = obj.custom.close : $$.bt = null;
+				obj.custom.keyIn('min') ? $$.m = obj.custom.min : $$.m = null;
+				obj.custom.keyIn('max') ? $$.mx = obj.custom.max : $$.mx = null;
+				obj.custom.keyIn('body') ? $$.bd = obj.custom.body : $$.bd = null;
+				obj.custom.keyIn('footer') ? $$.f = obj.custom.footer : $$.f = null;
+				document.body.appendChild(obj.custom.wrapper);
+			}else if(!obj.keyIn('custom')){
 				create();
-				return [d, h, bt, m, mx, bd, f];
-			}());
+				append();
+				document.body.appendChild($$.d);
+			}
+		}
+		
+		function ready(o){
+			appendToBody(o);
+			return [$$.d, $$.h, $$.bt, $$.m, $$.mx, $$.bd, $$.f];
+		}
 
-	},
+		return ready(obj);
 
-	toggle : function() {
-		var t = document.createElement('div');
-		t.setAttribute('class', 'dialog-toggle');
-		t.innerHTML = '+';
-		document.body.appendChild(t);
-		return t;
-	},
+	}
 
-	setClass : function(elements, obj) {
+	function toggle(obj) {
+		if(obj.keyIn('onside')){
+			if(isBool(obj.onside) && obj.onside == true){
+				$$.t = document.createElement('div');
+				$$.t.setAttribute('class', 'dialog-toggle');
+				$$.t.innerHTML = '+';
+				document.body.appendChild($$.t);
+			}else if(isObj(obj.onside)){
+				$$.t = obj.onside;
+				document.body.appendChild($$.t);
+			}
+		}
+	}
+
+	function setClass(elements, obj) {
 		if (obj.keyIn('css')) {
 			var css = obj.css;
 		}
@@ -67,155 +113,182 @@ var dialog = {
 				elements[i].setAttribute('class', css[i]);
 			}
 		}
-	},
+	}
 
-	appendBody : function(body, obj) {
-		if (obj.keyIn('html')) {
-			if ( typeof obj.html === 'string') {
-				var el = str2el(obj.html);
-				body.appendChild(el);
+	function appendBody(body, obj) {
+		if (obj.keyIn('body')) {
+			if ( typeof obj.body === 'string') {
+				body.appendChild(str2el(obj.body));
 			}
-			if ( typeof obj.html == 'object') {
-				if (obj.html) {
-					var el = obj.html.cloneNode(true);
-					body.appendChild(obj.html);
-				}
+			if ( typeof obj.body == 'object') {
+				body.appendChild(obj.body);
 			}
 		}
-	},
+	}
 
-	appendName : function(head, obj) {
+	function appendName(head, obj) {
 		if (obj.keyIn('title')) {
 			var n = document.createElement('h4');
 			n.appendChild(document.createTextNode(obj.title));
 			head.appendChild(n);
 		}
-	},
+	}
 
-	clearBody : function(el) {
+	function clearBody(el) {
 		emptyEl(el);
-	},
+	}
 
-	myEvents : function(obj) {
+	function myEvents(obj) {
 		if (obj.keyIn('acton')) {
 			for (var i = 0,
 			    j = obj.acton.myevent.length; i < j; i++) {
-				$(obj.acton.trigger[i]).addEventListener(obj.acton.myevent[i], obj.acton.handler[i], false);
-			};
+			    $(obj.acton.trigger[i]).addEventListener(obj.acton.myevent[i], obj.acton.handler[i], false);
+			}
 		}
-	},
+	}
 
-	init : function(trigger, obj) {
-		var single = counter();
+	function maximize() {
+		if (obj.keyIn('transform')) {
+			if ($$.d.className === obj.transform[0]) {
+				setClass([$$.d, $$.h, $$.bt, $$.m, $$.mx, $$.bd, $$.f], obj);
+				if ($$.t)
+					$$.t.style.display = 'none';
+			}
+		} else {
+			$$.d.style.display = 'block';
+			if ($$.t)
+				$$.t.style.display = 'none';
+		}
+	}
+
+	function minimize(array) {
+		if (obj.keyIn('transform')) {
+			if ($$.d.className === obj.css[0] || $$.d.className === 'dard_d_box') {
+				setClass([$$.d, $$.h, $$.bd, $$.f], obj.transform);
+				if ($$.t)
+					$$.t.style.display = 'block';
+			}
+		} else {
+			$$.d.style.display = 'none';
+			if ($$.t)
+				$$.t.style.display = 'block';
+		}
+	}
+
+	function touches(event) {
+		var touch = event.targetTouches[0];
+		$$.d.style.left = touch.pageX - 20 + 'px';
+		$$.d.style.top = touch.pageY - 20 + 'px';
+		event.preventDefault();
+	}
+
+	function getStyle(e, s) {
+		return window.getComputedStyle(e, null).getPropertyValue(s);
+	}
+
+	function mouseDown(e) {
+		window.addEventListener('mousemove', divMove, true);
+		y = e.clientY - getStyle($$.d, 'top').slice(0, -2),
+		x = e.clientX - getStyle($$.d, 'left').slice(0, -2);
+	}
+
+	function divMove(e) {
+		//d.style.position = 'absolute';
+		$$.d.style.top = (e.clientY - y) + 'px';
+		$$.d.style.left = (e.clientX - x) + 'px';
+	}
+
+	function freeup() {
+		window.removeEventListener('mousemove', divMove, true);
+		window.removeEventListener('touchmove', touches, true);
+	}
+
+	function remove() {
+		document.body.removeChild($$.d);
+		removeToggleButton();
+		single.reset();
+		removeDrag();
+		$$.bt.removeEventListener('click', remove, true);
+		removeMinButton();
+		removeMaxButton();
+	}
+	
+	function addToggleButton(){
+		if($$.t)
+			$$.t.addEventListener('click', maximize, false);
+	}
+	
+	function removeToggleButton(){
+		if($$.t)
+			document.body.removeChild($$.t);
+			$$.t.removeEventListener('click', maximize, true);
+	}
+	
+	function addMinButton(){
+		if($$.m)
+			$$.m.addEventListener('click', minimize, false);
+	}
+	
+	function addMaxButton(){
+		if($$.mx)
+			$$.mx.addEventListener('click', maximize, false);
+	}
+	
+	function removeMinButton(){
+		if($$.m)
+			$$.m.removeEventListener('click', minimize, true);
+	}
+	
+	function removeMaxButton(){
+		if($$.mx)
+			$$.mx.removeEventListener('click', maximize, true);
+	}
+	
+	function addDrag(){
+		if(drag){
+			$$.f.addEventListener('mousedown', mouseDown, false);
+			$$.h.addEventListener('mousedown', mouseDown, false);
+			window.addEventListener('mouseup', freeup, false);
+			$$.h.addEventListener('touchmove', touches, false);
+			window.addEventListener('touchend', freeup, false);
+		}
+	}
+	
+	function removeDrag(){
+		if(drag){
+			$$.h.removeEventListener('mousedown', mouseDown, true);
+			$$.f.removeEventListener('mousedown', mouseDown, true);
+			window.removeEventListener('mouseup', freeup, true);
+			window.removeEventListener('mousedown', freeup, true);
+		}
+	}
+
+	function init(trigger, obj) {
 		if (trigger) {
 			trigger.addEventListener('click', function() {
 				if (single.show() < 1) {
 					single.increment();
-					if (obj.sideButton)
-						var t = dialog.toggle();
-					var el = dialog.mywindow(),
-					    d = el[0],
-					    h = el[1],
-					    bt = el[2],
-					    m = el[3],
-					    mx = el[4],
-					    bd = el[5],
-					    f = el[6],
-					    x,
-					    y;
+					toggle(obj);
+					var el = mywindow(obj), x, y;
+					
+					setClass(el, obj);
+					appendName($$.h, obj);
+					appendBody($$.bd, obj);
 
-					dialog.setClass(el, obj);
-					dialog.appendName(h, obj);
-					dialog.appendBody(bd, obj);
-					//ontouchmove
+					addToggleButton();
+					addMaxButton();
+					addMinButton();
+					
+					$$.bt.addEventListener('click', remove, false);
 
-					if (t)
-						t.addEventListener('click', maximize, false);
-					m.addEventListener('click', minimize, false);
-					mx.addEventListener('click', maximize, false);
-					bt.addEventListener('click', remove, false);
+					addDrag();
 
-					f.addEventListener('mousedown', mouseDown, false);
-					h.addEventListener('mousedown', mouseDown, false);
-					window.addEventListener('mouseup', freeup, false);
-					h.addEventListener('touchmove', touches, false);
-					window.addEventListener('touchend', freeup, false);
-
-					function remove() {
-						document.body.removeChild(d);
-						if (obj.sideButton)
-							document.body.removeChild(t);
-						single.reset();
-						h.removeEventListener('mousedown', mouseDown, true);
-						f.removeEventListener('mousedown', mouseDown, true);
-						window.removeEventListener('mouseup', freeup, true);
-						window.removeEventListener('mousedown', freeup, true);
-						bt.removeEventListener('click', remove, true);
-						m.removeEventListener('click', minimize, true);
-					}
-
-					function maximize() {
-						if (obj.keyIn('transform')) {
-							if (d.className === obj.transform[0]) {
-								dialog.setClass(el, obj);
-								if (t)
-									t.style.display = 'none';
-							}
-						} else {
-							d.style.display = 'block';
-							if (t)
-								t.style.display = 'none';
-						}
-					}
-
-					function minimize() {
-						if (obj.keyIn('transform')) {
-							if (d.className === obj.css[0] || d.className === 'dard_d_box') {
-								dialog.setClass([d, h, bd, f], obj.transform);
-								if (t)
-									t.style.display = 'block';
-							}
-						} else {
-							d.style.display = 'none';
-							if (t)
-								t.style.display = 'block';
-						}
-					}
-
-					function touches(event) {
-						var touch = event.targetTouches[0];
-						d.style.left = touch.pageX - 20 + 'px';
-						d.style.top = touch.pageY - 20 + 'px';
-						event.preventDefault();
-					}
-
-					function getStyle(e, s) {
-						return window.getComputedStyle(e, null).getPropertyValue(s);
-					}
-
-					function mouseDown(e) {
-						window.addEventListener('mousemove', divMove, true);
-						y = e.clientY - getStyle(d, 'top').slice(0, -2),
-						x = e.clientX - getStyle(d, 'left').slice(0, -2);
-					}
-
-					function divMove(e) {
-						//d.style.position = 'absolute';
-						d.style.top = (e.clientY - y) + 'px';
-						d.style.left = (e.clientX - x) + 'px';
-					}
-
-					function freeup() {
-						window.removeEventListener('mousemove', divMove, true);
-						window.removeEventListener('touchmove', touches, true);
-					}
-
-
-					dialog.myEvents(obj);
+					myEvents(obj);
 				}
 			});
 		} else {
-			console.log(trigger + ' dialog content not on this page');
+			console.log(trigger + ' dialog button not on this page');
 		}
 	}
+	init(trigger, obj);
 };
